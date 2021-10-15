6ad4d3da043c9df45bf2023d2c6f75fd36992da3
Kevinmsouza <kevinm.souza@hotmail.com>
2021-10-15 03:50:52 -0400

    CRUD de Categorias implemetado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import pg from 'pg'
 import cors from 'cors'
+import joi from 'joi'
 
 const app = express();
 app.use(express.json())
@@ -16,6 +17,42 @@ const connection = new pg.Pool({
 
 app.get('/check_status', (req, res) => {
     res.send('Online')
+})
+
+app.get('/categories', async (req, res) => {
+    try {
+        const result = await connection.query('SELECT * FROM categories')
+        res.send(result.rows)
+    } catch (error){
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+app.post('/categories', async (req, res) => {
+    const categorySchema = joi.object({
+        name: joi.string()
+            .alphanum()
+            .max(30)
+            .required(),
+    })
+    const hasError = categorySchema.validate(req.body).error
+    if(hasError){
+        return res.sendStatus(400)
+    }
+
+    try {
+        const checkName= await connection.query('SELECT * FROM categories WHERE name = $1', [req.body.name])
+        if(checkName.rows.length){
+            return res.sendStatus(409)
+        }
+        await connection.query('INSERT INTO categories (name) VALUES ($1)', [req.body.name])
+        res.sendStatus(201)
+
+    }catch (error){
+        console.log(error)
+        res.sendStatus(500)
+    }
 })
 
 app.listen(4000)
